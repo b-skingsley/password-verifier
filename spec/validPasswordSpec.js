f5cea6808d7156602fcb293c7193a1196fd1623e
ben <benjaminskingsley@gmail.com>
2021-04-18 11:32:33 +0100

    Created password verifying js file and spec file, wrote out pseudocode for functionality and exported the function to the spec file

---
+++ spec/validPasswordSpec.js
@@ -0,0 +1,2 @@
+const validPassword = require("../validPassword")
+
---
+++ validPassword.js
@@ -0,0 +1,14 @@
+const validPassword = (password) => {
+  // check if the password follows the following verifications:
+
+  // The password should be larger than 8 chars
+
+  // The password should have one uppercase letter at least
+
+  // The password should have one lowercase letter at least
+
+  // The password should have one number at least
+
+};
+
+module.exports = validPassword;
