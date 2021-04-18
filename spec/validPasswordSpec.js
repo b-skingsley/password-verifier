23e6553e53f7d848b59546001a49ae9d2132b4d3
ben <benjaminskingsley@gmail.com>
2021-04-19 00:12:56 +0100

    Cleaned up code by removing the previous code that was commented out

--- spec/validPasswordSpec.js
+++ spec/validPasswordSpec.js
@@ -30,24 +30,6 @@ describe("validPassword", () => {
     }).toThrowError("Your password must satisfy at least 3 conditions");
   });
 
-  // it("should throw exception if password is less than 9 chars", () => {
-  //   expect(function() {
-  //     validPassword('pass');
-  //   }).toThrowError("Your password must be more than 8 characters");
-  // });
-
-  // it("should throw exception if password doesn't contain an uppercase letter", () => {
-  //   expect(function() {
-  //     validPassword('password1');
-  //   }).toThrowError("Your password must contain an uppercase letter");
-  // });
-
-  // it("should throw exception if password doesn't contain a number", () => {
-  //   expect(function() {
-  //     validPassword('PaSsWoRdS');
-  //   }).toThrowError("Your password must contain a number");
-  // });
-
   it("should return password is strong if all requirements are met", () => {
     const valid = validPassword('Password1');
     expect(valid).toBe("Password is Strong");
--- validPassword.js
+++ validPassword.js
@@ -1,16 +1,16 @@
 const validPassword = (password) => {
-  // We want to implement:
-  // - Password is OK if at least 3 of the initial 5 conditions is true
-  // - Password is never OK if item 4 is not true.
-  // If the password satisfies any of the conditions then it cannot be null, so the password must always satisfy not being null to be OK
-  // So if the password satisfies at least one of items 1, 3 or 5 then it will be OK
-
   // check if the password follows the following verifications:
   // 1. The password should be larger than 8 chars
   // 2. The password should not be null
   // 3. The password should have one uppercase letter at least
   // 4. The password should have one lowercase letter at least
   // 5. The password should have one number at least
+
+  // Additionally:
+  // Password is OK if at least 3 of the initial 5 conditions is true
+  // Password is never OK if item 4 is not true.
+  // If the password satisfies any of the conditions then it cannot be null, so the password must always satisfy not being null to be OK
+  // So if the password satisfies at least one of items 1, 3 or 5 then it will be OK
 
   // The password should not be null
   if (password === null || password === "") {
@@ -33,21 +33,6 @@ const validPassword = (password) => {
   }
 
   throw new Error("Your password must satisfy at least 3 conditions")
-
-  // // The password should be larger than 8 chars
-  // if (password.length <= 8) {
-  //   throw new Error("Your password must be more than 8 characters");
-  // }
-
-  // // The password should have one uppercase letter at least
-  // if (!/[A-Z]/.test(password)) {
-  //   throw new Error("Your password must contain an uppercase letter")
-  // }
-
-  // // The password should have one number at least
-  // if (!/[0-9]/.test(password)) {
-  //   throw new Error("Your password must contain a number")
-  // }
 };
 
 module.exports = validPassword;
