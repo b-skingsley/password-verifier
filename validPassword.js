bb7aae3034ec972f5582c6c349752b4d9862c957
ben <benjaminskingsley@gmail.com>
2021-04-18 17:13:23 +0100

    Wrote out pseudocode for approach to solving the additional requirements

--- validPassword.js
+++ validPassword.js
@@ -1,4 +1,10 @@
 const validPassword = (password) => {
+  // We want to implement:
+  // - Password is OK if at least 3 of the initial 5 conditions is true
+  // - Password is never OK if item 4 is not true.
+  // If the password satisfies any of the conditions then it cannot be null, so the password must always satisfy not being null to be OK
+  // So if the password satisfies at least one of items 1, 3 or 5 then it will be OK
+
   // check if the password follows the following verifications:
 
   // The password should not be null
@@ -25,6 +31,8 @@ const validPassword = (password) => {
   if (!/[0-9]/.test(password)) {
     throw new Error("Your password must contain a number")
   }
+
+  // If the password passes all verifications we return true
   return true
 };
 
