1cc2d8ec6e1e90d6dc72d911383e92c27f27137c
ben <benjaminskingsley@gmail.com>
2021-04-18 17:38:07 +0100

    Added tests for password being OK if at least 3 conditions are met, also changed return value for when all conditions are met to password being Strong. Also numbered the conditions in the pseudocode for easier referencing

--- spec/validPasswordSpec.js
+++ spec/validPasswordSpec.js
@@ -7,6 +7,23 @@ describe("validPassword", () => {
     }).toThrowError("You must provide a password");
   });
 
+  it("should throw exception if password doesn't contain a lowercase letter", () => {
+    expect(function() {
+      validPassword('PASSWORD1');
+    }).toThrowError("Your password must contain a lowercase letter");
+  });
+
+  it("should return password is OK if password satisfies at least 3 of the 5 conditions", () => {
+    // define constants for the validations of passwords that satisfy their respective condition (1, 3 and 5)
+    const validCondition1 = validPassword('passwords');
+    const validCondition3 = validPassword('Pass');
+    const validCondition5 = validPassword('pass1');
+
+    expect(validCondition1).toBe("Password is OK");
+    expect(validCondition3).toBe("Password is OK");
+    expect(validCondition5).toBe("Password is OK");
+  });
+
   it("should throw exception if password is less than 9 chars", () => {
     expect(function() {
       validPassword('pass');
@@ -19,20 +36,14 @@ describe("validPassword", () => {
     }).toThrowError("Your password must contain an uppercase letter");
   });
 
-  it("should throw exception if password doesn't contain a lowercase letter", () => {
-    expect(function() {
-      validPassword('PASSWORD1');
-    }).toThrowError("Your password must contain a lowercase letter");
-  });
-
   it("should throw exception if password doesn't contain a number", () => {
     expect(function() {
       validPassword('PaSsWoRdS');
     }).toThrowError("Your password must contain a number");
   });
 
-  it("should return true if password is valid", () => {
+  it("should return password is strong if all requirements are met", () => {
     const valid = validPassword('Password1');
-    expect(valid).toBe(true);
+    expect(valid).toBe("Password is Strong");
   });
 });
